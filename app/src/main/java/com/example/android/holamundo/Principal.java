dd8de00a8d4a1b8f70d9042d7cb6049de3ce695b
Aris Julio <ajulio5@cuc.edu.co>
2017-02-21 21:18:35 -0500

    Saludo realizado

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -2,12 +2,26 @@ package com.example.android.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private TextView lblMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        lblMessage = (TextView) findViewById(R.id.lblMessage);
+    }
+
+    public void saludar(View v) {
+        String aux = txtNombre.getText().toString();
+        lblMessage.setText("@string/saludo" + aux);
     }
 }
