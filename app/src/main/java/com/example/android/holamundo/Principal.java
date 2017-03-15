b1f2dd766f9815f9c5a7527835460be62d952c14
Daniel Nieto <android@cuc.edu.co>
2017-03-14 20:38:35 -0500

    Saludo completado y bilingue

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -1,5 +1,6 @@
 package com.example.android.holamundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,21 +8,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
-
-    private EditText txtNombre;
+//declaramos variable
+    private EditText cajaNombre;
+    private EditText cajaApellido;
     private TextView lblMessage;
-
+    private Intent i;
+    private Bundle b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
-        txtNombre = (EditText) findViewById(R.id.txtNombre);
-        lblMessage = (TextView) findViewById(R.id.lblMessage);
+//capturamos las cajas
+        cajaNombre = (EditText) findViewById(R.id.txtNombre);
+        cajaApellido = (EditText) findViewById(R.id.txtApellido);
+        //creamos el onjeto intet que sirve para pasar de una actividad a otra
+        i = new Intent(this,Saludo.class);
+        //se cra el objeto bundle que sirver para encapsular datos
+        b = new Bundle();
     }
 
     public void saludar(View v) {
-        String aux = txtNombre.getText().toString();
-        lblMessage.setText("Hola " + aux);
+        String nomb,apell;
+        //tomo el valor que la persona ingreso tanto en nombre como en apellido
+        nomb = cajaNombre.getText().toString();
+        apell= cajaApellido.getText().toString();
+        //Encapsulo los valores previamente tomados
+        b.putString("Nombre",nomb);
+        b.putString("Apellido",apell);
+        //le paso al intent todos los datos en forma encapsulada con el bundle
+        i.putExtras(b);
+        //Arranco la actividad que el intent me diga
+        startActivity(i);
+      /*  String aux = txtNombre.getText().toString();
+        lblMessage.setText("Hola " + aux);*/
     }
 }
--- app/src/main/java/com/example/android/holamundo/Saludo.java
+++ app/src/main/java/com/example/android/holamundo/Saludo.java
@@ -1,13 +1,33 @@
 package com.example.android.holamundo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Saludo extends AppCompatActivity {
 
+    private TextView saludo;
+    private  Bundle b;
+    private String aux,nomb,apell;
+    private Resources res;
+
+
     @Override
+    //on create es lo primero que se ejecuta
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+
+        saludo = (TextView)findViewById(R.id.txtSaludo);
+        b= getIntent().getExtras();
+        nomb = b.getString("Nombre");
+        apell=b.getString("Apellido");
+        res = this.getResources();
+
+        aux=res.getString(R.string.saludo)+" "+nomb+" "+apell+" "+res.getString(R.string.partesaludo2);
+        saludo.setText(aux);
     }
 }
